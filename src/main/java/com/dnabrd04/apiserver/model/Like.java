574b7e8fa7df4dc9badaf8b88a1c9b3ab5e4cf9c
dnabr04 <diegomatnf2004@gmail.com>
2025-04-06 17:09:50 +0200

    Added the construtors and getters and setters on Like class

--- src/main/java/com/dnabrd04/apiserver/model/Like.java
+++ src/main/java/com/dnabrd04/apiserver/model/Like.java
@@ -3,7 +3,8 @@ package com.dnabrd04.apiserver.model;
 import jakarta.persistence.*;
 
 /**
- * This class will be used to pass data from the "Post" table to objects.
+ * This class will be used to pass data from the "Like" table to objects.
+ * This class will store the id, the user and the post.
  *
  * @author dnabrd04
  */
@@ -21,4 +22,42 @@ public class Like {
     @ManyToOne
     @JoinColumn(name = "id_post", foreignKey = @ForeignKey(name = "FK_LIKE_POST"), nullable = false)
     private Post post;
+
+    public Like() {
+    }
+
+    public Like(Long id_like, User user, Post post) {
+        this.id_like = id_like;
+        this.user = user;
+        this.post = post;
+    }
+
+    public Like(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
+
+    public Long getId_like() {
+        return id_like;
+    }
+
+    public void setId_like(Long id_like) {
+        this.id_like = id_like;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
